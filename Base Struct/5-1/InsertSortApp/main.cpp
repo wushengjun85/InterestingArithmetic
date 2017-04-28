709354fd8d91ffa97a3268e9ef1b6f2447a31b96
吴胜军 <wushengjun85@163.com>
2017-04-28 13:37:15 +0800

    InsertSortApp
    InsertSortApp

---
+++ Base Struct/5-1/InsertSortApp/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+void insertSort(int a[],int n)
+{
+    int key = 0;
+    int i;
+    int j;
+    while(i<n)
+    {
+        key = a[i];
+        j = i - 1;
+        while(j>= 0 && key < a[j])
+        {
+            a[j+1] = a[j];
+            j = j -1;
+        }
+        a[j + 1] = key;
+        i++;
+    }
+}
+int main(int argc, char *argv[])
+{
+    //cout << "Hello World!" << endl;
+    int data[10];
+    int i;
+    cout<<"请输入10个整数："<<endl;
+    for(i = 0; i < 10; i++)
+        cin>>data[i];
+    cout<<"您输入的值为："<<endl;
+    for(i = 0; i < 10; i++)
+        cout<<data[i]<<" ";
+    cout<<endl;
+    insertSort(data,10);
+    cout<<"排序后的元素顺序为："<<endl;
+    for(i= 0; i < 10; i++)
+        cout<<data[i]<<" ";
+    return 0;
+}
