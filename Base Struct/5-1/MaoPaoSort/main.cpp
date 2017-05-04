cdae1960b75f5e13ba910a136a70e3473c361c47
吴胜军 <wushengjun85@163.com>
2017-05-04 08:29:29 +0800

    MaoPaoSort
    first commit

---
+++ Base Struct/5-1/MaoPaoSort/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+void bubbleSort(int [],int);
+void swap(int *,int *);
+
+/********************交换函数**************************/
+void swap(int *a,int *b)
+{
+    *a = *a^*b;
+    *b = *a^*b;
+    *a = *a^*b;
+}
+/*******************冒泡排序***************************/
+void bubbleSort(int data[],int n)
+{
+    int flag;
+    int i = 1,j;
+    do
+    {
+        flag = 0;
+        for(j = 0; j + 1 < n; j++)
+        {
+            if(data[j] > data[j + 1])
+            {
+                swap(data[j],data[j+1]);
+                flag = 1;
+            }
+        }
+        i++;
+    }while(i < n && flag == 1);
+}
+int main(int argc, char *argv[])
+{
+    //cout << "Hello World!" << endl;
+    int data[10] = {23,43,23,19,87,65,78,90,7,10};
+    cout<<"排序前的数组为: "<<endl;
+    for(int i = 0; i < 10; i++)
+        cout<<data[i]<<" ";
+    cout<<endl;
+    bubbleSort(data,10);
+    cout<<"排序后的数组为: "<<endl;
+    for(int i = 0; i < 10; i++)
+        cout<<data[i]<<" ";
+    cout<<endl;
+    return 0;
+}
